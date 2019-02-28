bb1932037732ffef0d2dd7bde8fbb238ccfb6c02
guofuu <2118065565@qq.com>
2019-02-28 19:56:43 +0800

    [新增]

---
+++ test_2_5/test_2_5/test.c
@@ -0,0 +1,31 @@
+# define _CRT_SECURE_NO_WARNINGS 2
+
+# include<stdio.h>
+
+
+int main()
+{
+
+ //char arr1[]="abcdef";
+ //char arr2[]={'a','b','c','d','e','f','\n'};
+ //printf("%s\n",arr1);
+ //printf("%s\n",arr2);
+
+printf("\a\a\a\a\a\\a\");
+ 
+
+
+  //int num=20;
+  //scanf("%d",&num);
+  //printf("num=%d\n",num);
+
+ 
+//printf("hehe\n");
+//printf("hehe\n");
+//printf("hehe\n");
+//printf("hehe\n");
+//printf("hehe\n");
+return 0;
+
+
+}
