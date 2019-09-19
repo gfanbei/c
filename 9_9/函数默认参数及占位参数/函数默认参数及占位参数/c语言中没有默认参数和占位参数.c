445e47457b3ab0921fe00ff538d33c5d80cd4478
gfanbei <2118065565@qq.com>
2019-09-20 07:56:57 +0800

    [新增]

---
+++ 9_9/函数默认参数及占位参数/函数默认参数及占位参数/c语言中没有默认参数和占位参数.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#if 0
+//c中没有默认参数 没有占位参数
+void fun1(int a = 1 ,int)
+{
+   
+}
+void test03()
+{
+	fun1();
+}
+
+#endif
+
+int main2()
+{
+	//test03();
+	return 0;
+}
